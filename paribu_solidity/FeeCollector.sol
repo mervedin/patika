fc5a6a3e500d5a0349335ce1fab0ee448a39bb42
Merve Din <merve.din16@gmail.com>
2023-01-07 18:52:07 +0300

    fee collector

---
+++ paribu_solidity/FeeCollector.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract FeeCollector {
+    address public owner;
+    uint public balance;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    // when the contract receives eth, add that amount tot the balance
+    receive() payable external{
+        balance += msg.value;
+    }
+
+    // withdraw eth from the contract, only the contract owner can withdraw
+    function withdraw(address payable destAddr, uint amount) public {
+        require(owner == msg.sender, "Only owner can withdraw");
+        require(amount <= balance, "Insufficient funds");
+        
+        destAddr.transfer(amount);
+        balance -= amount;
+    }
+
+}
